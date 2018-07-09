15734d048ee4509bc19ad8434e0ed910b471e5d4
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-07-08 21:38:44 -0700

    server is up and running

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'))
+
+app.listen(PORT, () => {
+    console.log('Currently listening on port:', PORT);
+});
