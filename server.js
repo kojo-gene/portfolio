c695528d1a506af4bdcb5b25e46fe399cbdcc4a0
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-08-11 22:03:59 -0700

    deploying online

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'))
+app.use(express.static('./public'));
+
+app.get('/', function(request, response) {
+    response.sendFile('./public/index.html')
+})
 
 app.listen(PORT, () => {
     console.log('Currently listening on port:', PORT);
